1cc2e2ac77c7a450645acf83c3c4f5726c27eda0
dothnguyen <dothnguyen@gmail.com>
2020-05-17 14:55:43 +1000

    adding wayne-blog project

---
+++ apps/wayne-blog/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wayne-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'wayne-blog';
+}
